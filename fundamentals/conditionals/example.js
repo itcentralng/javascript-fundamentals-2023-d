6ca3b9e64476c58acd737ea8dfad40d989837417
Nasir Mustapha <teboy.com@gmail.com>
2023-07-17 13:02:51 +0100

    intorduction to conditional statements

---
+++ fundamentals/conditionals/example.js
@@ -0,0 +1,23 @@
+let num1 = 5;
+let num2 = 6;
+
+if (num1 < num2){
+    console.log('Yup! num2 is bigger');
+}else if (num1 == 5){
+    console.log('Yup! num1 is smaller');
+}else{
+    console.log('No condition was met');
+}
+
+// Indepents
+if (num1 < num2){
+    console.log('Yup! num2 is bigger');
+}
+if (num1 < num2){
+    console.log('Yup! num2 is bigger');
+}
+
+// Exercise
+// Create a shopping list with 5 items, each items with a name and price
+// Using your previous understanding of arrays and objects,
+// Show the items we can afford assuming we have 500 to spend.
