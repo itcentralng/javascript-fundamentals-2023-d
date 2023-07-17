a7a274958eb538ec1435468bf69e8ba8f8e2d589
Nasir Mustapha <teboy.com@gmail.com>
2023-07-17 12:23:20 +0100

    Create a chatbot

---
+++ fundamentals/exercises/4.js
@@ -0,0 +1,3 @@
+// Q1: Create a chatbot that users can talk with via the terminal. The chatbot
+// should be able to respond to questions from users, the response should
+// be random but still meaningful.
