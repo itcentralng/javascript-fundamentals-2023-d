76638c37a2cebeeb3d068fa0e86cc02168db7466
Nasir Mustapha <teboy.com@gmail.com>
2023-07-18 10:48:57 +0100

    more conditional exercise

--- fundamentals/conditionals/example.js
+++ fundamentals/conditionals/example.js
@@ -18,6 +18,18 @@ if (num1 < num2){
 }
 
 // Exercise
-// Create a shopping list with 5 items, each items with a name and price
+// Q1: Create a shopping list with 5 items, each items with a name and price
 // Using your previous understanding of arrays and objects,
 // Show the items we can afford assuming we have 500 to spend.
+
+// Q2: Check from a list fruits if it contains banana and print it out. If
+// it doesnt, print the last fruit on the list.
+
+// Q3: Check from a string, if it starts with a vowel, print "WISH" if
+// it starts with a consonant print "WASH" instead. But if it starts with
+// a vowel and ends with a consonant print "WISHWASH" and if it starts with
+// a consonant and ends with a vowel then print "WASHWISH"
+
+// Q4: Check from a list of people, if there are an even number of people,
+// print the two people in the middle, if there are odd number of people, 
+// print the only person in the middle.
